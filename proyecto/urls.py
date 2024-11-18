16ac0a5a2560d867131b5c846d42f17bc3cab18d
EzequielLassalle <ezelassalle9@gmail.com>
2024-11-18 11:38:21 -0300

    Avances

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,31 +1,7 @@
-"""
-URL configuration for proyecto project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-from django.urls import path, include
-from aplicacion import views
+from django.urls import path, include  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/datos/', views.obtener_datos, name='obtener_datos'),  
-    path('api/producto/<int:pk>/', views.obtener_producto, name='obtener_producto'),  
-    path('api/producto/', views.crear_producto, name='crear_producto'),  
-    path('api/producto/<int:pk>/', views.actualizar_producto, name='actualizar_producto'),  
-    path('api/producto/<int:pk>/eliminar/', views.eliminar_producto, name='eliminar_producto'),  
     path('api/', include('aplicacion.urls')),  
 ]
-
